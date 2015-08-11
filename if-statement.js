75d17a8627ef4069fb446bc49090b6d57b0a518d
Alexis Ibarra <ar.ibarrasalas@gmail.com>
2015-08-11 13:51:50 -0430

    Añadiendo los scripts del workshop javascripting

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+var fruit = 'orasssge';
+
+if (fruit > 5) {
+  console.log('The fruit name has five characters or less');
+} else {
+  console.log('The fruit name has more than five characters.');
+};
+
